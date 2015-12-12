a75c77eddf407436e0f2d031493f651a2330c1a3
David Cook <divergentdave@gmail.com>
2015-12-11 23:37:47 -0600

    Add rudimentary setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import setuptools
+from distutils.core import setup
+
+setup(
+  name = 'dwgstrings',
+  version = '0.1',
+  packages = ['dwgstrings'],
+  entry_points = {
+    'console_scripts': [
+      'dwgstrings = dwgstrings.__main__:main'
+    ]
+  }
+)
